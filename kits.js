33a72d2474f7e3a768621737dc81039d36ab50d6
龙骨 <1308957543@qq.com>
2019-08-20 19:29:21 +0800

    新增一个十六进制的随机颜色

--- kits.js
+++ kits.js
@@ -28,7 +28,7 @@ kits.randomInt = function(n,m){
   return Math.floor(Math.random() * (m-n+1) + n);
 }
 
-// 常见的给id的方式1
+// 常见的给id的方式
 // 当前时间戳 + 大的随机数
 kits.getId = function(){
   // 返回一个不容易重复的id
@@ -49,3 +49,22 @@ kits.randomcolor=function (){
   var b = randomInt (0,255);
   return 'rgb(' + r + ',' + g + ',' + b + ')'; 
 }
+
+// 封装一个十六进制的随机颜色
+kits.getColor =function(){
+  //定义字符串变量colorValue存放可以构成十六进制颜色值的值
+  var colorValue="0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+  //以","为分隔符，将colorValue字符串分割为字符数组["0","1",...,"f"]
+  var colorArray = colorValue.split(",");
+   var color="#";//定义一个存放十六进制颜色值的字符串变量，先将#存放进去
+    //使用for循环语句生成剩余的六位十六进制值
+   for(var i=0;i<6;i++){
+        //colorArray[Math.floor(Math.random()*16)]随机取出
+       // 由16个元素组成的colorArray的某一个值，然后将其加在color中，
+       //字符串相加后，得出的仍是字符串
+        color += colorArray[Math.floor(Math.random()*16)];
+   }
+   return color;
+}
+
+
